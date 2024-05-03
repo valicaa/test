e250e35e579e71da8307453525b9baeb98d59142
valicaa <xoffel2000@gmail.com>
2024-05-03 18:05:24 +0700

    Change FPS to 144

--- desktop/src/com/mygdx/bomberman/DesktopLauncher.java
+++ desktop/src/com/mygdx/bomberman/DesktopLauncher.java
@@ -8,7 +8,7 @@ import com.mygdx.bomberman.Bomberman;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+		config.setForegroundFPS(144);
 		config.setTitle("Bomberman");
 		new Lwjgl3Application(new Bomberman(), config);
 	}
